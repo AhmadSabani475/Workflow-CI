b6dcee7f9e618157c985cb4206962d9bfbec826a
AhmadSabani475 <sabanirizki275@gmail.com>
2026-06-08 10:52:42 +0700

    fix modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -14,20 +14,11 @@ y = df['Sleep Disorder']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-input_example = X_train[0:5]
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run():
-    model = RandomForestClassifier(n_estimators=500, max_depth=40, random_state=42)
-    model.fit(X_train, y_train)
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
+model = RandomForestClassifier(n_estimators=500, max_depth=40, random_state=42)
+model.fit(X_train, y_train)
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-    )
-
-    print(f"Accuracy: {accuracy}")
+accuracy = model.score(X_test, y_test)
+print(f"Accuracy: {accuracy}")
