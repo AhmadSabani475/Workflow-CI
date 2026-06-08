ae0d80baa0874cefff61b8288ac5cff73117ce21
AhmadSabani475 <sabanirizki275@gmail.com>
2026-06-08 10:41:27 +0700

    fix kriteria 3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,11 +1,10 @@
 import mlflow
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import numpy as np
-import dagshub
 
-dagshub.init(repo_owner='AhmadSabani475', repo_name='Eksperimen_SML_Ahmad-Rizki-Sabani', mlflow=True)
+mlflow.set_tracking_uri('https://dagshub.com/AhmadSabani475/Eksperimen_SML_Ahmad-Rizki-Sabani.mlflow')
 mlflow.set_experiment("Eksperimen_kriteria3")
 
 df = pd.read_csv("data_preprocessing.csv")
@@ -20,10 +19,7 @@ input_example = X_train[0:5]
 mlflow.sklearn.autolog()
 
 with mlflow.start_run():
-    n_estimators = 500
-    max_depth = 40
-
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+    model = RandomForestClassifier(n_estimators=500, max_depth=40, random_state=42)
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
     mlflow.log_metric("accuracy", accuracy)
@@ -33,3 +29,5 @@ with mlflow.start_run():
         artifact_path="model",
         input_example=input_example
     )
+
+    print(f"Accuracy: {accuracy}")
