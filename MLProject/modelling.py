bdf60ac3cd87b4f6516d31cbca1ab15bac950c5e
AhmadSabani475 <sabanirizki275@gmail.com>
2026-06-08 10:59:00 +0700

    add artifact

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
+import joblib
 mlflow.set_tracking_uri('https://dagshub.com/AhmadSabani475/Eksperimen_SML_Ahmad-Rizki-Sabani.mlflow')
 mlflow.set_experiment("Eksperimen_kriteria3")
 
@@ -22,3 +22,6 @@ model.fit(X_train, y_train)
 
 accuracy = model.score(X_test, y_test)
 print(f"Accuracy: {accuracy}")
+
+joblib.dump(model, 'model.pkl')
+print("Model saved to model.pkl")
